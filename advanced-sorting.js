cda8a06dce51001f64d01f20a6b9656216c18f9b
Eduardo Go <eduardo.go.rj@gmail.com>
2017-06-29 16:12:10 +0000

    Sort students by name

---
+++ advanced-sorting.js
@@ -0,0 +1,21 @@
+var students = [
+  {id: 1, name: "bruce", age: 40},
+  {id: 2, name: "zoidberg", age: 22},
+  {id: 3, name: "alex", age: 22},
+  {id: 4, name: "alex", age: 30}
+];
+
+function compare(a, b) {
+  var nameA = a.name;
+  var nameB = b.name;
+
+  if(nameA < nameB) {
+    return -1;
+  } else if(nameA > nameB) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+console.log(students.sort(compare));
