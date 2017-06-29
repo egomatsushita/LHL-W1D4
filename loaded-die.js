15e0778981e86023fe17e76768a14b32f8a6a8c8
Eduardo Go <eduardo.go.rj@gmail.com>
2017-06-29 19:07:14 +0000

    Add makeLoadedDie function

---
+++ loaded-die.js
@@ -0,0 +1,21 @@
+var rolldie = function() {
+  return Math.floor(1 + Math.random() * 6);
+}
+
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var index = 0;
+
+  return function() {
+
+    return list[index++];
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+
