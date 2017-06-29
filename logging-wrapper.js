b583d62b5a6ed80c1bb575b20a685d76f2347cfa
Eduardo Go <eduardo.go.rj@gmail.com>
2017-06-29 20:16:11 +0000

    Using arguments[0] to call callback arguments

--- logging-wrapper.js
+++ logging-wrapper.js
@@ -1,5 +1,18 @@
 var wrapLog = function(callback, name) {
+  var name = name;
+  var info = "";
+  return function() {
+    var x = arguments[0];
+    var y = arguments[1];
 
+    if(name === "area") {
+      info = `area(${x}, ${y}) => ${area(x, y)}`;
+    } else if(name === "volume") {
+      var z = arguments[2];
+      info = `volume(${x}, ${y}, ${z}) => ${volume(x, y, z)}`;
+    }
+    return info;
+  };
 };
 
 var area = function(x, y) {
@@ -8,8 +21,8 @@ var area = function(x, y) {
 
 var logArea = wrapLog(area, "area");
 
-logAra(5, 3); // area(5, 3) => 15
-logAra(3, 2); // area(3, 2) => 6
+console.log(logArea(5, 3)); // area(5, 3) => 15
+console.log(logArea(3, 2)); // area(3, 2) => 6
 
 var volume = function(x, y, z) {
   return x * y * z;
@@ -17,5 +30,5 @@ var volume = function(x, y, z) {
 
 var logVolume = wrapLog(volume, "volume");
 
-logVolume(5, 3, 2); // volume(5, 3, 2) => 20
-logVolume(3, 2, 4); // volume(3, 2, 4) => 24
+console.log(logVolume(5, 3, 2)); // volume(5, 3, 2) => 30
+console.log(logVolume(3, 2, 4)); // volume(3, 2, 4) => 24
